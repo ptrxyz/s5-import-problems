4365e1f62550bb194b148cb47e8b975ca8c864d2
Peter Krauß <peter.krauss@kit.edu>
2024-11-03 13:12:47 +0100

    cleanup

--- src/lib/test/test.svelte
+++ src/lib/test/test.svelte
@@ -1,15 +1,11 @@
 <script lang="ts" module>
-	import type { Component } from 'svelte';
-	import button from './button.svelte';
+	import button from './button.svelte'; // typed as: (alias) const button: Component<Props, {}, "">
 	export type ButtonInstance = InstanceType<typeof button>; // it IS NOT a class in Svelte files
 	export type ButtonFunction = ReturnType<typeof button>; // it IS a function in Svelte files
 
 	import type { ComponentProps } from 'svelte';
-	export type PropsHelper<C> = C extends Component<infer P> ? P : never;
-
 	import type { Props } from './button.svelte'; // I CAN import types from Svelte files into Svelte files
 	export type ButtonProps = ComponentProps<typeof button>['onclick']; // works
-	export type ButtonPropsInferred = PropsHelper<typeof button>['onclick']; // works, but I have to create a helper
 	export type ButtonPropsAsImported = Props['onclick']; // works, but I have to reexport Props all the time
 
 	export type ButtonPropsFromInstance = ButtonInstance['onclick']; // wrongly typed as any
--- src/lib/test/test.ts
+++ src/lib/test/test.ts
@@ -1,14 +1,10 @@
-import type { Component } from 'svelte';
-import button from './button.svelte';
+import button from './button.svelte'; // typed as: import button
 export type ButtonInstance = InstanceType<typeof button>; // it IS a class in ts files
 export type ButtonFunction = ReturnType<typeof button>; // it IS NOT a function in ts files
 
 import type { ComponentProps } from 'svelte';
-export type PropsHelper<C> = C extends Component<infer P> ? P : never;
-
-import type { Props } from './button.svelte'; // I CAN NOT import types from Svelte files into Svelte files
-export type ButtonProps = ComponentProps<typeof button>['onclick']; // works
-export type ButtonPropsInferred = PropsHelper<typeof button>['onclick']; // doesn't work
+import type { Props } from './button.svelte'; // I CAN NOT import types from Svelte files into Svelte files (error: no exported member 'Props')
+export type ButtonProps = ComponentProps<typeof button>['onclick']; // doesn't work, typeof button does not satisfy SvelteComponent
 export type ButtonPropsAsImported = Props['onclick']; // doesn't work, since the import failed anyway
 
 export type ButtonPropsFromInstance = ButtonInstance['onclick']; // property doesnt exist
